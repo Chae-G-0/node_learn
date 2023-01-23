fabed5494ed6dfd93374dacca3d2bb54ab25d858
Chae-G-0 <corkdud@naver.com>
2023-01-24 00:36:54 +0900

    230124

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended:true}))
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(8080, function () {
-  console.log("listening on 8080");
-});
+const MongoClient = require("mongodb").MongoClient;
+
+let db;
+MongoClient.connect(
+  "mongodb+srv://gy1024:rkdud4020@cluster0.oemqahq.mongodb.net/node_learn?retryWrites=true&w=majority",
+  function (error, client) {
+    if (error) return console.log(error);
+
+    db = client.db('node_learn')
+
+    db.collection('post').insertOne({_id:100, name: "kim", age:28}, function (erroe, res) {
+      console.log('저장완료')
+    });
+
+    app.listen("8080", function () {
+      console.log("listening on 8080");
+    });
+  }
+);
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -16,6 +32,6 @@ app.get("/write", function (req, res) {
 });
 
 app.post("/add", function (req, res) {
-  res.send('ok')
-  console.log(req.body.title)
-})
+  res.send("ok");
+  console.log(req.body.title);
+});
