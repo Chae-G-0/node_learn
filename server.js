26f25512de438f0e054b1527820a544f8814386d
Chae-G-0 <corkdud@naver.com>
2023-01-23 23:56:34 +0900

    230123

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.listen(8080, function () {
   console.log("listening on 8080");
 });
 
-app.get("/beauty", function (req, res) {
-  res.send("뷰티용품 판매 페이지");
-});
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + "/index.html");
 });
+
+app.get("/write", function (req, res) {
+  res.sendFile(__dirname + "/write.html");
+});
+
+app.post("/add", function (req, res) {
+  res.send('ok')
+  console.log(req.body.title)
+})
